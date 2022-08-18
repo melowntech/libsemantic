7cd54d000605824e73d02e9c69721918f9c767b5
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2022-08-18 03:15:04 +0200

    windows build

--- semantic/mesh/tree.cpp
+++ semantic/mesh/tree.cpp
@@ -56,10 +56,16 @@ geometry::Mesh newMeshIcosahedronRaw()
     geometry::Mesh mesh;
 
     // https://www.danielsieger.com/blog/2021/01/03/generating-platonic-solids.html
-
+    
+#ifdef _WIN32
+    const double phi = (1.0 + sqrt(5.0)) * 0.5; // golden ratio
+    const double a = 1.0;
+    const double b = 1.0 / phi;
+#else
     constexpr double phi = (1.0f + sqrt(5.0f)) * 0.5f; // golden ratio
     constexpr double a = 1.0f;
     constexpr double b = 1.0f / phi;
+#endif
 
     mesh.vertices.emplace_back(math::normalize(math::Point3(0, +b, -a)));
     mesh.vertices.emplace_back(math::normalize(math::Point3(+b, +a, 0)));
